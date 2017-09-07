ece859d1bf8760de86ede037ed1f11e7bca0bec5
Colin Mollenhour <colin@mollenhour.com>
2017-09-07 02:24:21 -0400

    Fix default baseURL and set default proxyTable to match backend.

--- config/index.js
+++ config/index.js
@@ -44,7 +44,15 @@ module.exports = {
     // Proxy your API if using any.
     // Also see /build/script.dev.js and search for "proxy api requests"
     // https://github.com/chimurai/http-proxy-middleware
-    proxyTable: {}
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:8000/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ if (__THEME === 'mat') {
 
 Vue.use(Quasar) // Install Quasar Framework
 
-axios.defaults.baseURL = 'http://localhost/api/v1'
+axios.defaults.baseURL = 'http://localhost:8080/api/v1'
 // Check if user is logged in or not + refresh token
 auth.checkAuth(this)
 
